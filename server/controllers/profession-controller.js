dd4817d5f0c686759252ebdcea2df88e7fcc71c9
ameena <mairaj.ameena@siemens.com>
2020-03-17 19:42:38 +0530

    Changes : Add Collection and Update Collection

--- server/controllers/profession-controller.js
+++ server/controllers/profession-controller.js
@@ -1,33 +1,43 @@
-const configureDB = require('../config/arangoDB-config');
+const configureDB = require("../config/arangoDB-config");
 const db = configureDB();
-const careerCollection = db.collection('career');
+const careerCollection = db.collection("career");
 
-exports.getProfessionList = (req,res) =>{
+exports.getProfessionList = (req, res) => {
   careerCollection
-  .all()
-  .then((response) => {
-    console.log(response)
-    return res.status(200).json(response._result);
-  })
-  .catch((ex)=>{
-    return res.status(500).json(error);
-  });
+    .all()
+    .then(response => {
+      // console.log(response)
+      return res.status(200).json(response._result);
+    })
+    .catch(error => {
+      return res.status(500).json(error);
+    });
 };
 
-exports.getProfessionById = (req,res) => {
+exports.getProfessionById = (req, res) => {
   console.log(req);
-}
-
-exports.addProfession = (req,res) => {
-    // TO DO : Add career values
-  console.log(req);
-
 };
 
-exports.deleteProfession = (req,res) => {
-  console.log(req);
-}
+exports.addProfession = (req, res) => {
+  // TO DO : Add career values
+  careerCollection
+    .save(req.body)
+    .then(response => res.status(200).json(response))
+    .catch(error => {
+      return res.status(500).json(error);
+    });
+};
 
-exports.updateProfession = (req,res) => {
+exports.deleteProfession = (req, res) => {
   console.log(req);
-}
+};
+
+exports.updateProfession = (req, res) => {
+  let key = req.body._key;
+  careerCollection
+    .update(key, req.body)
+    .then(response => res.status(200).json(response))
+    .catch(error => {
+      return res.status(500).json(error);
+    });
+};
--- server/routes/profession-routes.js
+++ server/routes/profession-routes.js
@@ -1,10 +1,9 @@
+const professionController = require("../controllers/profession-controller");
 
-const professionController = require('../controllers/profession-controller');
-
-module.exports = function(app){
-    app.get('/api/professions', professionController.getProfessionList);
-    app.get('/api/profession/:id', professionController.getProfessionById);
-    app.post('/api/professions',professionController.addProfession);
-    app.put('/api/professions/:id', professionController.updateProfession);
-    app.delete('/api/profession/:id',professionController.deleteProfession);
-}
+module.exports = function(app) {
+  app.get("/api/professions", professionController.getProfessionList);
+  app.get("/api/professions/:id", professionController.getProfessionById);
+  app.post("/api/professions", professionController.addProfession);
+  app.put("/api/professions/:id", professionController.updateProfession);
+  app.delete("/api/professions/:id", professionController.deleteProfession);
+};
